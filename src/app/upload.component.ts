790f8c201433fdd291aa8ca0679f3bbed89ddf19
EPICOScode <sagarcia612@mail.com>
2023-07-28 12:03:57 +0200

    beginning with the component

---
+++ src/app/upload.component.ts
@@ -0,0 +1,12 @@
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'upload',
+    template:'./app.component.html',
+})
+
+
+export class uploadcomponent{
+
+}
